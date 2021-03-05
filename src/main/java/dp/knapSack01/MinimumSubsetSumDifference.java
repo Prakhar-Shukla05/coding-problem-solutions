fd5c186776f3cf87713e992946a11ac6775bbb93
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-03-06 01:31:30 +0530

    Optimised Minimum Subset Sum Difference problem

--- src/main/java/dp/knapSack01/MinimumSubsetSumDifference.java
+++ src/main/java/dp/knapSack01/MinimumSubsetSumDifference.java
@@ -13,53 +13,53 @@ Subset2 = {11}, sum of Subset2 = 11
 public class MinimumSubsetSumDifference {
 
 	public int minDiffernce(int arr[], int n) {
-		
-		int sum=0;
-		for(int x : arr)
-			sum+=x;
-		
-		int mid=sum/2;
-		int minDifference=sum;
-		for(int i=mid;i<=sum;i++) {
-			if(hasSubsetSum(i,arr,n)) {
-				minDifference=Math.abs(i-(sum-i));
+
+		int sum = 0;
+		for (int x : arr)
+			sum += x;
+
+		int mid = sum / 2;
+		int minDifference = sum;
+		boolean[][] dp = new boolean[n + 1][sum + 1];
+		hasSubsetSum(sum, arr, n, dp);
+		for (int i = mid; i >= 0; i--) {
+			if (dp[n][i]) {
+				minDifference = sum - 2 * i;
 				break;
 			}
 		}
 		return minDifference;
-	} 
-	
-	private boolean hasSubsetSum(int target, int [] arr, int n) {
-		
-		boolean [][] dp= new boolean [n+1][target+1];
-		for(int i=0;i<=n;i++) {
-			for(int j=0;j<=target;j++) {
-				if(i==0)
-					dp[i][j]=false;
-				if(j==0)
-					dp[i][j]=true;
+	}
+
+	private boolean hasSubsetSum(int target, int[] arr, int n, boolean[][] dp) {
+
+		// boolean [][] dp= new boolean [n+1][target+1];
+		for (int i = 0; i <= n; i++) {
+			for (int j = 0; j <= target; j++) {
+				if (i == 0)
+					dp[i][j] = false;
+				if (j == 0)
+					dp[i][j] = true;
 			}
 		}
-		
-		for(int i=1;i<=n;i++) {
-			for(int j=1;j<=target;j++) {
-				if(arr[i-1]<=j) {
-					dp[i][j]=  dp[i-1][j-arr[i-1]] || dp[i-1][j];
-				}
-				else
-					dp[i][j]= dp[i-1][j];
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= target; j++) {
+				if (arr[i - 1] <= j) {
+					dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
+				} else
+					dp[i][j] = dp[i - 1][j];
 			}
 		}
-		
+
 		return dp[n][target];
-		
-		
+
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int [] arr= {1,2,7};
-		System.out.println(new MinimumSubsetSumDifference().minDiffernce(arr, 3));
+		int[] arr = { 1, 6, 11, 5 };
+		System.out.println(new MinimumSubsetSumDifference().minDiffernce(arr, 4));
 
 	}
 
