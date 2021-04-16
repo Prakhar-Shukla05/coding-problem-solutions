a58a12d6b8c4bd371c8cc75ed0cac3d7e5d8e01b
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-04-16 19:16:33 +0530

    Added Diameter of a binary tree

---
+++ src/main/java/dp/trees/DiameterOfABinaryTree.java
@@ -0,0 +1,56 @@
+package dp.trees;
+
+//GeeksforGeeks
+//Problem link- https://practice.geeksforgeeks.org/problems/diameter-of-binary-tree/1
+//Given a Binary Tree, find diameter of it.
+//The diameter of a tree is the number of nodes on the longest path between two end nodes in the tree. The diagram below shows two trees each with diameter nine, the leaves that form the ends of a longest path are shaded (note that there is more than one path in each tree of length nine, but no path longer than nine nodes).
+//Input:
+//    1
+//  /  \
+// 2    3
+//Output: 3
+
+public class DiameterOfABinaryTree {
+
+	static int result =1;
+	static int diameter(Node root) {
+		
+		if(root==null) {
+			return 0;
+		}
+		solve(root);
+		return result;
+    }
+	
+	private static int solve(Node root) {
+		
+		if(root==null)
+			return 0;
+		
+		int l= solve(root.left);
+		int r= solve(root.right);
+		int temp= Math.max(l, r)+1;	
+		result= Math.max(1+l+r,result);
+		return temp;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class Node
+{
+	int val;
+	Node left;
+	Node right;
+	
+	public Node(int val, dp.trees.Node left, dp.trees.Node right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+	
+	
+}
