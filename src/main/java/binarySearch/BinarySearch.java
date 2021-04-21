4b5f9558cc93ee1e7b992d81a6057d1526090ad7
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-04-22 00:58:54 +0530

    Added some more problems

--- src/main/java/binarySearch/BinarySearch.java
+++ src/main/java/binarySearch/BinarySearch.java
@@ -2,6 +2,21 @@ package binarySearch;
 
 public class BinarySearch {
 
+	public static int bs(int [] arr,int start, int end, int target) {
+		
+		while(start<=end) {
+			int mid= start+(end-start)/2;
+			if(arr[mid]==target)
+				return mid;
+			else if(arr[mid]>target) {
+				end=mid-1;
+			}
+			else if(arr[mid]<target) {
+				start=mid+1;
+			}
+		}
+		return -1;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
