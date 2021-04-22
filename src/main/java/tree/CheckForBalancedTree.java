272488f8eec7ecc46924b1c27eedec85b020b286
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-04-22 10:34:59 +0530

    Added more problems

--- src/main/java/tree/CheckForBalancedTree.java
+++ src/main/java/tree/CheckForBalancedTree.java
@@ -7,42 +7,28 @@ public class CheckForBalancedTree {
 		if(root==null)
 			return true;
 		
-		boolean answer=true;
-		height(root,answer);
-		return answer;
+		int lh= height(root.left);
+		int rh= height(root.right);
+		if(Math.abs(lh-rh)<=1 && isBalanced(root.left) && isBalanced(root.right))
+			return true;
+		return false;
     }
 	
-	private static int height(Node root, boolean result) {
+	private static int height(Node root) {
 		
 		if(root==null)
 			return 0;
 		
-		int left= height(root.left,result);
-		int right= height(root.right,result);
-		if(Math.abs(left-right)>1)
-		{
-			result=false;
-		}
+		int left= height(root.left);
+		int right= height(root.right);
+		
 		return Math.max(left, right)+1;
 		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Node root = new Node(9);
-		root.left= new Node(7);
-		root.right= null;
-		root.left.left= new Node(8);
-		root.left.right= new Node(10);
-		root.right.left= null;
-		root.right.right= new Node(6);
-		root.left.left.left= new Node(4);
-		root.left.left.right= null;
-		root.left.right.left= new Node(6);
-		root.left.right.right= new Node(10);
-		root.left.left.left.left= null;
-		root.left.left.left.right= new Node(8);
-		System.out.println(new CheckForBalancedTree().isBalanced(root));
-
+		
+	
 	}
 
 }
