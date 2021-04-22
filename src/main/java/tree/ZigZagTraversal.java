046ce51d7a19c8cca716f0a924e663b440da59e1
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-04-22 09:55:49 +0530

    Added Zig Zag Traversal

--- src/main/java/tree/ZigZagTraversal.java
+++ src/main/java/tree/ZigZagTraversal.java
@@ -1,7 +1,47 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ZigZagTraversal {
 
+	
+	ArrayList<Integer> zigZagTraversal(Node root)
+	{
+	    ArrayList<Integer> result= new ArrayList<>();
+	    if(root==null)
+	    	return result;
+	    
+	    Queue<Node> queue= new LinkedList<>();
+	    boolean reversed=false;
+	    queue.add(root);
+	    while(!queue.isEmpty()) {
+	    	ArrayList<Integer> level= new ArrayList<>();
+	    	int size=queue.size();
+	    	while(size>0) {
+	    		Node temp= queue.poll();
+	    		level.add(temp.data);
+	    		if(temp.left!=null)
+	    			queue.add(temp.left);
+	    		if(temp.right!=null)
+	    			queue.add(temp.right);
+	    		size--;
+	    	}
+	    	if(reversed) {
+	    		Collections.reverse(level);
+	    		for(int i : level)
+	    			result.add(i);
+	    	}
+	    	else {
+	    		for(int i: level)
+	    			result.add(i);
+	    	}
+	    	reversed=!reversed;
+	    }
+	    return result;
+	}	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
