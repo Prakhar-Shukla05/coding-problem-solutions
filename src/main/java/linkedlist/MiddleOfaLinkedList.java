17d6e4145a8f138049ae8ad64acaa3206a8472d6
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-05-04 17:48:39 +0530

    Added middle of linked list

---
+++ src/main/java/linkedlist/MiddleOfaLinkedList.java
@@ -0,0 +1,25 @@
+package linkedlist;
+
+public class MiddleOfaLinkedList {
+
+	public ListNode middleNode(ListNode head) {
+            if(head==null)
+            	return null;
+            
+            ListNode slow=head;
+            ListNode fast= head;
+            while(fast.next!=null && fast.next.next!=null) {
+            	slow=slow.next;
+            	fast=fast.next.next;
+            }
+            
+            return fast.next==null?slow:slow.next;
+            	
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
