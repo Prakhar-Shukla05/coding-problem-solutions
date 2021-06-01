723232fbb10142b852fcbc4e94aba2ec20b7c6f4
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-06-01 12:23:43 +0530

    Modified count of times array is rotated

--- src/main/java/binarySearch/CountHowManyTimesArrayIsRotated.java
+++ src/main/java/binarySearch/CountHowManyTimesArrayIsRotated.java
@@ -6,13 +6,7 @@ public class CountHowManyTimesArrayIsRotated {
 		
 		if(arr==null || arr.length==0)
 			return 0;
-		if(arr.length==1)
-			return 0;
-		if(arr.length==2) {
-			if(arr[0]>arr[1])
-				return 1;
-			else return 0;
-		}
+		
 		int count= bfs(arr,n);
 		return count;
 	}
@@ -20,27 +14,35 @@ public class CountHowManyTimesArrayIsRotated {
 	private static int bfs(int [] arr, int n) {
 		int start= 0;
 		int end=n-1;
+		if(arr[start]<=arr[end])
+			return 0;
+		int pivot=-1;
 		while(start<end) {
+			if(arr[start]<=arr[end]) {
+				pivot=start;
+				break;
+            }
 			int mid=start+(end-start)/2;
-			if(arr[mid]>arr[mid+1])
-				return mid+1;
-			else if(mid !=0 && arr[mid]<arr[mid-1])
-			{
-				return mid;
+			
+			int next= (mid+1)%n;
+			int prev= (mid-1+n)%n;
+			if(arr[mid]<=arr[next] && arr[mid]<= arr[prev]) {
+				pivot=mid;
+				break;
 			}
-			else if(arr[mid]>arr[end])
+			else if(arr[start]<=arr[mid])
 				start=mid+1;
-			else {
+			else if(arr[mid]<=arr[end])
 				end=mid-1;
-			}
+			
 		}
 		
-		return 0;
+		return pivot;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       int [] arr = {4,5,1,2,3};
+       int [] arr = {2,3,4,5,1};
        int n=5;
        System.out.println(new CountHowManyTimesArrayIsRotated().findKRotation(arr, n));
 	}
