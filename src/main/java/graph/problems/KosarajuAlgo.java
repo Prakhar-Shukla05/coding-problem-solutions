bb5b9cf1e165ade902fa646cecd2805bf4873eed
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-07-07 01:26:04 +0530

    Added Kosaraju Algo problem

--- src/main/java/graph/problems/KosarajuAlgo.java
+++ src/main/java/graph/problems/KosarajuAlgo.java
@@ -1,53 +1,53 @@
-package graph.algorithms;
+package graph.problems;
 
 import java.util.ArrayList;
 import java.util.Stack;
 
-//A directed graph is strongly connected if there is a path between all pairs of
-//vertices. A strongly connected component (SCC) of a directed graph is a maximal 
-//strongly connected subgraph. For example, there are 3 SCCs in the following graph.
+//GFG
+//Problem link- https://practice.geeksforgeeks.org/problems/strongly-connected-components-kosarajus-algo/1
+//Strongly Connected Components (Kosaraju's Algo) 
+//Medium Accuracy: 49.73% Submissions: 25612 Points: 4
+//Given a Directed Graph with V vertices (Numbered from 0 to V-1) and E edges,
+//Find the number of strongly connected components in the graph.
 
-//1. Sort the nodees in order of finishing time i.e. topological sort
-//2. Tranpose the graph (Reverse the edges)
-//3. Perform dfs according to finishing time
+public class KosarajuAlgo {
 
-public class KosarajuAlgoForStronglyConnectedComponents {
-
-	public void kosaRaju(ArrayList<ArrayList<Integer>> adj, int n) {
-
+	public int kosaraju(int V, ArrayList<ArrayList<Integer>> adj) {
 		Stack<Integer> st = new Stack<Integer>();
-		boolean visited[] = new boolean[n];
-		for (int i = 0; i < n; i++) {
+		boolean visited[] = new boolean[V];
+		for (int i = 0; i < V; i++) {
 			if (!visited[i])
 				dfs(i, st, adj, visited);
 		}
 		ArrayList<ArrayList<Integer>> transpose = new ArrayList<ArrayList<Integer>>();
 
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < V; i++)
 			transpose.add(new ArrayList<Integer>());
 
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < V; i++) {
 			visited[i] = false;
 			for (Integer it : adj.get(i)) {
 				transpose.get(it).add(i);
 			}
 		}
 
+		int ans=0;
 		while (!st.isEmpty()) {
 			int top = st.peek();
 			st.pop();
 			if (!visited[top]) {
-				System.out.print("SCC is: ");
+				//System.out.print("SCC is: ");
+				ans++;
 				revDfs(top, visited, transpose);
-				System.out.println();
+				//System.out.println();
 			}
 		}
+		return ans;
 	}
-
 	private void revDfs(int top, boolean[] visited, ArrayList<ArrayList<Integer>> transpose) {
 		// TODO Auto-generated method stub
 		visited[top] = true;
-		System.out.print(top + " ");
+		//System.out.print(top + " ");
 		for (Integer it : transpose.get(top)) {
 			if (!visited[it])
 				revDfs(it, visited, transpose);
@@ -68,7 +68,6 @@ public class KosarajuAlgoForStronglyConnectedComponents {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		int n = 5;
 		ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
 
@@ -80,7 +79,8 @@ public class KosarajuAlgoForStronglyConnectedComponents {
 		adj.get(2).add(0);
 		adj.get(1).add(3);
 		adj.get(3).add(4);
-		new KosarajuAlgoForStronglyConnectedComponents().kosaRaju(adj, n);
+		
+		System.out.println(new KosarajuAlgo().kosaraju(n, adj));
 
 	}
 
