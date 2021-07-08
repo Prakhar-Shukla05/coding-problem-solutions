bda9170ba5b74ffed538afe0e9ada696b6105267
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-07-08 12:08:41 +0530

    Added more problems

--- src/main/java/graph/problems/RedundantConnection.java
+++ src/main/java/graph/problems/RedundantConnection.java
@@ -1,5 +1,75 @@
 package graph.problems;
 
+//Leetcode 684
+
+//Problem link-https://leetcode.com/problems/redundant-connection/
+//In this problem, a tree is an undirected graph that is connected and has no cycles.
+//
+//You are given a graph that started as a tree with n nodes labeled from 1 to n,
+//with one additional edge added. The added edge has two different vertices chosen
+//from 1 to n, and was not an edge that already existed. The graph is represented 
+//as an array edges of length n where edges[i] = [ai, bi] indicates that there is 
+//an edge between nodes ai and bi in the graph.
+//
+//Return an edge that can be removed so that the resulting graph is a tree of n nodes.
+//If there are multiple answers, return the answer that occurs last in the input.
+//		
+//
+//		Example 1:
+//
+//
+//			Input: edges = [[1,2],[1,3],[2,3]]
+//			Output: [2,3]
+//		
+
+//We can solve this using Disjoint Set data structure
 public class RedundantConnection {
+
+	private int findPar(int p, int [] parent) {
+		if(p==parent[p])
+			return p;
+		else
+			return parent[p]=findPar(parent[p],parent);
+	}
+	
+	private void union(int p, int q,int [] parent,int [] rank) {
+		int pr=findPar(p,parent);
+		int qr=findPar(q,parent);
+		if(pr==qr) return ;
+		if(rank[pr]<rank[qr]) {
+			parent[pr]=qr;
+		}
+		else if(rank[pr]>rank[qr]) {
+			parent[qr]=pr;
+		}
+		else {
+			parent[qr]=pr;
+			rank[pr]++;
+		}
+		
+		
+	}
+	public int[] findRedundantConnection(int[][] edges) {
+		
+		int n= edges.length;
+		int [] parent = new int [n+1];
+		int [] rank = new int [n+1];
+		for (int i=0;i<=n;i++) {
+			parent[i]=i;
+		}
+		int [] res= new int [2];
+		for(int [] edge : edges) {
+			int u=edge[0];
+			int v=edge[1];
+			if(findPar(u,parent)==findPar(v,parent)) {
+				res[0]=u;
+				res[1]=v;
+				
+			}
+			union(u,v,parent,rank);
+		}
+		return res;
+
+	}
 
 }
--- src/main/java/slidingwindow/fixed/MaximumOfAllSubarraysOfSizeK.java
+++ src/main/java/slidingwindow/fixed/MaximumOfAllSubarraysOfSizeK.java
@@ -1,10 +1,98 @@
 package slidingwindow.fixed;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
+//InterviewBit
+//Problem link-https://www.interviewbit.com/problems/sliding-window-maximum/
+//
+//
+//is moving from the very left of the array to the very right. 
+//
+//You can only see the w numbers in the window. Each time the sliding window moves 
+//
+//rightwards by one position. You have to find the maximum for each window. 
+//
+//The following example will give you more clarity.
+//
+//The array A is [1 3 -1 -3 5 3 6 7], and B is 3.
+//
+//Window position	Max
+//———————————-	————————-
+//[1  3  -1] -3  5  3  6  7	3
+//1 [3  -1  -3] 5  3  6  7	3
+//1  3 [-1  -3  5] 3  6  7	5
+//1  3  -1 [-3  5  3] 6  7	5
+//1  3  -1  -3 [5  3  6] 7	6
+//1  3  -1  -3  5 [3  6  7]	7
+//Return an array C, where C[i] is the maximum value of from A[i] to A[i+B-1].
+//
+//Note: If B > length of the array, return 1 element with the max of the array.
+//
+//
+//
+//
+//Input Format
+//
+//The first argument given is the integer array A.
+//The second argument given is the integer B.
+//Output Format
+//
+//Return an array C, where C[i] is the maximum value of from A[i] to A[i+B-1]
+//For Example
+//
+//Input 1:
+//    A = [1, 3, -1, -3, 5, 3, 6, 7]
+//    B = 3
+//Output 1:
+//    C = [3, 3, 5, 5, 6, 7]
 
 public class MaximumOfAllSubarraysOfSizeK {
 
+	public int[] slidingMaximum(final int[] A, int B) {
+		int [] ans= new int [A.length-B+1];
+		int i=0;
+		int j=0;
+		Deque<Integer> dq= new LinkedList<Integer>();
+		int index=0;
+		while(j<A.length) {
+			if(dq.isEmpty())
+				dq.addLast(A[j]);
+		    else if(!dq.isEmpty()&& A[j]>dq.getLast()) {
+				while(!dq.isEmpty() && dq.getLast()<A[j])
+					dq.pollLast();
+				dq.addLast(A[j]);
+			}
+		    else {
+		    	dq.addLast(A[j]);
+		    }
+			if(j-i+1<B) j++;
+			
+			else if(j-i+1==B) {
+				ans[index++]=dq.getFirst();
+				
+				if(A[i]==dq.getFirst())
+					dq.pollFirst();
+				
+				i++;
+				j++;
+			}
+			
+		}
+		return ans;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int [] ans= {648, 614, 490, 138, 657, 544, 745, 582, 738, 229, 775, 665, 876, 448, 4, 81, 807, 578, 712, 951, 867, 328, 308, 440, 542, 178, 637, 446, 882, 760, 354, 523, 935, 277, 158, 698, 536, 165, 892, 327, 574, 516, 36, 705, 900, 482, 558, 937, 207, 368 };
+		int k=9;
+		int [] res= new MaximumOfAllSubarraysOfSizeK().slidingMaximum(ans, k);
+		int [] expected = {745, 745 ,775 ,775 ,876, 876 ,876 ,876 ,876 ,876 ,876 ,951 ,951 ,951 ,951, 951 ,951 ,951 ,951 ,951 ,882 ,882 ,882 ,882 ,935 ,935 ,935 ,935 ,935 ,935 ,935 ,935 ,935 ,892 ,892 ,892 ,900 ,900 ,900 ,937, 937 ,937 };
+		for(int i=0;i<res.length;i++) {
+			if(res[i]!=expected[i])
+				System.out.println("Wrong");
+		}
+		System.out.println("Correct");
 
 	}
 
