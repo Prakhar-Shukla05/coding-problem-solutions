cec6ee136119c3e9b74b853a399b5010c31a4d67
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-07-10 14:37:47 +0530

    Added Count Invesions

---
+++ src/main/java/arrays/CountInversions.java
@@ -0,0 +1,93 @@
+package arrays;
+
+import tree.InvertABinaryTree;
+
+//GFG
+//Problem link-https://practice.geeksforgeeks.org/problems/inversion-of-array-1587115620/1
+//
+//Given an array of integers. Find the Inversion Count in the array. 
+//
+//Inversion Count: For an array, inversion count indicates how far (or close) the array is from being sorted. If array is already sorted then the inversion count is 0. If an array is sorted in the reverse order then the inversion count is the maximum. 
+//Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.
+// 
+//
+//Example 1:
+//
+//Input: N = 5, arr[] = {2, 4, 1, 3, 5}
+//Output: 3
+//Explanation: The sequence 2, 4, 1, 3, 5 
+//has three inversions (2, 1), (4, 1), (4, 3).
+//Example 2:
+//
+//Input: N = 5
+//arr[] = {2, 3, 4, 5, 6}
+//Output: 0
+//Explanation: As the sequence is already 
+//sorted so there is no inversion count.
+//Example 3:
+//
+//Input: N = 3, arr[] = {10, 10, 10}
+//Output: 0
+//Explanation: As all the elements of array 
+//are same, so there is no inversion count.
+
+public class CountInversions {
+
+	static long inversionCount(long arr[], long N)
+    {
+        long [] countInversions= new long[1];
+        sort(arr,countInversions);
+        return countInversions[0];
+    }
+	private static void sort(long[] arr, long[] countInversions) {
+		// TODO Auto-generated method stub
+		long [] aux= new long[arr.length];
+		sort(arr,aux,0,arr.length-1,countInversions);
+		
+	}
+	private static void sort(long[] arr, long[] aux, int low, int hi, long[] countInversions) {
+		// TODO Auto-generated method stub
+		
+		if(hi<=low) return ;
+		
+		int mid= low+(hi-low)/2;
+		sort(arr,aux,low,mid,countInversions);
+		sort(arr,aux,mid+1,hi,countInversions);
+		if(!less(arr,mid+1,mid))
+			return ;
+		merge(arr,aux,low,mid,hi,countInversions);
+		
+	}
+	private static void merge(long[] arr, long[] aux, int low, int mid, int hi, long[] countInversions) {
+		// TODO Auto-generated method stub
+		int i=low;
+		int j=mid+1;
+		int k;
+		for(k=low;k<=hi;k++)
+			aux[k]=arr[k];
+		
+		for(k=low;k<=hi;k++) {
+			if(i>mid) arr[k]=aux[j++];
+			else if(j>hi) arr[k]=aux[i++];
+			else if(less(aux,j,i)) {
+				countInversions[0]+=j-k;
+				arr[k]=aux[j++];
+			}
+			else {
+				arr[k]=aux[i++];
+			}
+		}
+		
+	}
+	private static boolean less(long[] arr, int i, int j) {
+		// TODO Auto-generated method stub
+		return arr[i]<arr[j];
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        long [] arr= {2,4,1,3,5};
+        int N=5;
+        System.out.println(inversionCount(arr, N));
+	}
+
+}
--- src/main/java/arrays/LongestConsecutiveSequence.java
+++ src/main/java/arrays/LongestConsecutiveSequence.java
@@ -1,5 +1,79 @@
 package arrays;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+//Leetcode 128
+//Problem link-https://leetcode.com/problems/longest-consecutive-sequence/
+//Given an unsorted array of integers nums, return the length of the longest 
+//consecutive elements sequence.
+//
+//You must write an algorithm that runs in O(n) time.
+//
+// 
+//
+//Example 1:
+//
+//Input: nums = [100,4,200,1,3,2]
+//Output: 4
+//Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+//Example 2:
+//
+//Input: nums = [0,3,7,2,5,8,4,6,0,1]
+//Output: 9
 public class LongestConsecutiveSequence {
+
+	// Sort the array and the find the max consecutive length
+	// Time complexity -o(nlogn)
+	public int longestConsecutiveWithSorting(int[] nums) {
+		if (nums.length == 0 || nums.length == 1)
+			return nums.length;
+		Arrays.sort(nums);
+		int max = 1;
+		int curr = 1;
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] == nums[i - 1] + 1) {
+				curr++;
+				max = Math.max(max, curr);
+			} else if (nums[i] != nums[i - 1]) {
+				max = Math.max(max, curr);
+				curr = 1;
+			}
+		}
+		return max;
+	}
+
+	//Using HashSet. Time Complexity -O(n)
+	//Enter all the elements in a set
+	//then iterate throigh the array again and if the num-1 is present then continue
+	// the goal is to find the lowest element in the consecutive order and from there finding the sequence
+	public int longestConsecutive(int[] nums) {
+		 
+		if (nums.length == 0 || nums.length == 1)
+			return nums.length;
+		
+		Set<Integer> set= new HashSet<>();
+		for(int i : nums)
+			set.add(i);
+		
+
+		int max=1;
+		
+		for(int num : nums) {
+			
+			if(!set.contains(num-1)) {
+				int currentNum=num;
+				int currentStreak=1;
+				while(set.contains(currentNum+1)) {
+					currentStreak++;
+					currentNum++;
+				}
+				max=Math.max(max,currentStreak);
+			}
+		}
+		return max;
+
+	}
 
 }
